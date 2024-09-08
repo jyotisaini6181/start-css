21a9efc186c0f6c3b50c7e6e85716e4ef8aa603a
Jyoti Saini <jyotisaini6181@gmail.com>
2024-09-08 21:10:40 +0530

    profile changes

--- profile.js
+++ profile.js
@@ -3,12 +3,23 @@ const unauthorizedMenu = document.getElementById("unauthorized");
 const profileMenu = document.getElementById("profile-menu");
 const signInModal = document.getElementById("signInModal");
 
+const usernameInput = document.getElementById("username-input");
+const emailInput = document.getElementById("email-input");
+const passwordInput = document.getElementById("password-input");
+
 function logout() {
   profileMenu.innerHTML = unauthorizedMenu.innerHTML;
 }
 
 function login() {
   profileMenu.innerHTML = profileCard.innerHTML;
+
+  const usernameDisplay = document.getElementById("username-display");
+  const emailDisplay = document.getElementById("email-display");
+
+  usernameDisplay.innerHTML = usernameInput.value;
+  emailDisplay.innerHTML = emailInput.value;
+
   signInModal.close();
 }
 
